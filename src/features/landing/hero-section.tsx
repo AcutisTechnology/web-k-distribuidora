da161844555d29b8b625b2ccd10564b6c270cd8d
sergiofilhoja <sergiofilhoja@hotmail.com>
2025-04-08 12:04:36 -0300

    Add new logo and update HeroSection layout and content for improved messaging

--- src/features/landing/hero-section.tsx
+++ src/features/landing/hero-section.tsx
@@ -1,51 +1,66 @@
 import { Stack, Target, ShieldCheck } from "@phosphor-icons/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export function HeroSection() {
+  const router = useRouter();
   return (
     <div className="relative flex flex-col lg:flex-row justify-between mx-auto px-6 py-8 bg-[url('/images/hero-background.png')] bg-cover bg-center">
       <div className="container flex flex-col lg:flex-row justify-between mx-auto px-6 py-14 w-full">
         <div className="flex flex-col lg:flex-row gap-14">
-          <div className="lg:max-w-xl justify-start items-start flex flex-col text-left z-10">
+          <div className="lg:max-w-xl justify-start items-start flex flex-col text-left">
             <div className="w-32 h-1 bg-[#141414] mb-6" />
-            <h1 className="font-syne font-bold text-3xl lg:text-5xl text-[#141414] mb-4 whitespace-nowrap">
-              A escolha de quem
+            <h1 className="font-syne font-bold text-3xl lg:text-5xl text-[#141414] mb-2">
+              Feitos para
             </h1>
-            <h1 className="font-syne font-bold text-3xl lg:text-5xl text-[#141414] mb-4">
-              Valoriza qualidade.
-            </h1>
+            <div className="relative">
+              <h1 className="font-syne font-bold text-3xl lg:text-5xl text-[#141414] mb-4">
+                Especialistas em Beleza
+              </h1>
+            </div>
             <p className="text-[#606060] font-montserrat mb-8">
               Condições exclusivas para cabeleireiros, barbearias e salões.
             </p>
 
             {/* Botão personalizado */}
-            <a
-              href="#diferenciais"
-              className="flex items-center justify-center px-8 py-3 mb-12 bg-black text-white rounded-full cursor-pointer transition-colors"
+            <div
+              onClick={() => router.push("/cadastro")}
+              className="flex items-center justify-center px-8 py-3 mb-12 bg-[#a89777] text-white rounded-lg cursor-pointer hover:bg-[#97876a] transition-colors"
             >
               <span className="font-montserrat font-medium text-sm">
                 Veja nossos diferenciais
               </span>
-            </a>
+            </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
               <div className="bg-white p-4 rounded-lg flex flex-col items-center text-center shadow-sm">
                 <ShieldCheck className="text-[#141414] mb-2" size={24} />
                 <p className="font-montserrat text-sm">
-                  Distribuidor oficial Wella Professionals na Paraíba
+                  Distribuidora oficial Wella
                 </p>
               </div>
               <div className="bg-white p-4 rounded-lg flex flex-col items-center text-center shadow-sm">
                 <Target className="text-[#141414] mb-2" size={24} />
-                <p className="font-montserrat text-sm">Suporte e treinamento</p>
+                <p className="font-montserrat text-sm">
+                  Lançamento em primeira mão
+                </p>
               </div>
               <div className="bg-white p-4 rounded-lg flex flex-col items-center text-center shadow-sm">
                 <Stack className="text-[#141414] mb-2" size={24} />
                 <p className="font-montserrat text-sm">
-                  Entrega rápida e segura
+                  Exclusivo para profissionais de beleza
                 </p>
               </div>
             </div>
+
+            {/* Logo */}
+            <Image
+              src="/images/logo-marrom.svg"
+              width={90}
+              height={90}
+              alt="Logo K Distribuidora"
+              className="max-h-[50px] sm:max-h-[60px] md:max-h-[65px] lg:max-h-[80px] mt-2"
+            />
           </div>
 
           <div className="absolute right-0 top-0 bottom-0 w-[500px] bg-[#a89777] rounded-l-full opacity-90 -z-10" />
